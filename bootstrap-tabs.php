2336e8e4ac7106294df24aa5e0cc9e897f188b3a
pavitravandehoven <pavitra@hotmail.com>
2018-03-26 15:22:21 +0200

    update2
    boostrap-tab

---
+++ bootstrap-tabs.php
@@ -0,0 +1,43 @@
+<html>
+  
+  <head>
+     
+   <title> input database producten</title>
+        
+ 
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ 
+   </head>
+    
+ 
+   <body>
+      
+  <nav>
+ 
+ <div class="nav nav-tabs" id="nav-tab" role="tablist">
+ 
+   <a class="nav-item nav-link active" id="nav-input-tab" data-toggle="input" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Input-formulier</a>
+  
+  <a class="nav-item nav-link" id="nav-delete-tab" data-toggle="tab" href="#nav-delete" role="tab" aria-controls="nav-delete" aria-selected="false">delete-table</a>
+  
+  <a class="nav-item nav-link" id="nav-update-tab" data-toggle="tab" href="#nav-update" role="tab" aria-controls="nav-update" aria-selected="false">update-table</a>
+   
+  <a class="nav-item nav-link" id="nav-lijst-tab" data-toggle="tab" href="#nav-lijst" role="tab" aria-controls="nav-lijst" aria-selected="false">lijst</a>
+ 
+ </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="nav-input" role="tabpanel" aria-labelledby="nav-input-tab">...</div>
+ 
+ <div class="tab-pane fade" id="nav-delete" role="tabpanel" aria-labelledby="nav-delete-tab">...</div>
+  
+<div class="tab-pane fade" id="nav-update" role="tabpanel" aria-labelledby="nav-update-tab">...</div>
+ 
+
+  <div class="tab-pane fade" id="nav-lijst" role="tabpanel" aria-labelledby="nav-lijst-tab">...</div>
+
+</div>
+    
+</body>
+
+</html>
